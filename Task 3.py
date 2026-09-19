72e1eeefffa29fa11d048d2bf0e32beddac21516
strofimcuk815-alt <strofimcuk815@gmail.com.ua>
2026-09-19 20:45:32 +0300

    Task 3

---
+++ Task 3.py
@@ -0,0 +1,23 @@
+import random
+number=random.randint(1,10)
+your_number=int(input("Спробуйте відгадати число від 1 до 10: "))
+if your_number==number:
+    print("Правильно!")
+elif your_number>number:
+    print("Ваше число більше за загадане")
+else:
+    print("Ваше число менше за загадане")
+your_number=int(input("Спробуйте відгадати число від 1 до 10: "))
+if your_number==number:
+    print("Правильно!")
+elif your_number>number:
+    print("Ваше число більше за загадане")
+else:
+    print("Ваше число менше за загадане")
+your_number=int(input("Спробуйте відгадати число від 1 до 10: "))
+if your_number==number:
+    print("Правильно!")
+elif your_number>number:
+    print("Ви програли")
+else:
+    print("Ви програли")
